3f57e1318990f2b9193973ffe90bab20f266e262
Hf1b <itzdaniel1337@gmail.com>
2020-11-17 12:15:00 +0500

    build.sh circleci support & one-style edit

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ PROJECT_NAME="Eureka Kernel"
 CORES=$(nproc --all)
 SELINUX_STATUS=""
 TYPE="oneui"
-REV=1.0
+REV=5.0
 USER=Chatur
 ZIPNAME=Eureka_Rx.x_Axxx_xxxx_x_x.zip
 DEFAULT_NAME=Eureka_Rx.x_Axxx_P/Q/R
@@ -99,37 +99,31 @@ CLEAN_SOURCE()
 	echo "*****************************************************"
 	make clean
 	CLEAN_SUCCESS=$?
-	if [ $CLEAN_SUCCESS != 0 ]
-		then
-			echo " Error: make clean failed"
-			exit
+	if [ $CLEAN_SUCCESS != 0 ]; then
+		echo " Error: make clean failed"
+		exit
 	fi
 
 	make mrproper
 	MRPROPER_SUCCESS=$?
-	if [ $MRPROPER_SUCCESS != 0 ]
-		then
-			echo " Error: make mrproper failed"
-			exit
+	if [ $MRPROPER_SUCCESS != 0 ]; then
+		echo " Error: make mrproper failed"
+		exit
 	fi
-	
+
 	if [ -e "kernel_zip/anykernel/Image" ]
 	then
-	  {
-	     rm $DTB_DIR/*.dtb
-	     rm $DTBO_DIR/*.dtbo
-	     rm -rf kernel_zip/anykernel/Image
-	     rm -rf kernel_zip/anykernel/dtbo.img
-	  }
+		rm $DTB_DIR/*.dtb
+		rm $DTBO_DIR/*.dtbo
+		rm -rf kernel_zip/anykernel/Image
+		rm -rf kernel_zip/anykernel/dtbo.img
 	fi
 	
 	if [ -e "kernel_zip/aroma/aroma_files/$AROMA_DIR/Image" ]
 	then
-	  {
-	     rm -rf kernel_zip/aroma/aroma_files/$AROMA_DIR/Image
-	     rm -rf kernel_zip/aroma/aroma_files/$AROMA_DIR/$AROMA_ZIP
-	     rm -rf kernel_zip/aroma/dtbo/main/dtbo.img
-	  }
+		rm -rf kernel_zip/aroma/aroma_files/$AROMA_DIR/Image
+		rm -rf kernel_zip/aroma/aroma_files/$AROMA_DIR/$AROMA_ZIP
+		rm -rf kernel_zip/aroma/dtbo/main/dtbo.img
 	fi
 	sleep 1	
 }
@@ -149,42 +143,38 @@ AUTO_TOOLCHAIN()
 {
 	if [ -e "toolchain/linaro6.5" ]
 	then
-	  {
-	     echo " "
-	     echo "Using Linaro v6.5 toolchain"
-	     echo " "
-	     GCC_ARM64_FILE=aarch64-linux-gnu-
-	     GCC_ARM32_FILE=arm-linux-gnueabi-
-	     export CROSS_COMPILE=$(pwd)/toolchain/bin/$GCC_ARM64_FILE
-	     export CROSS_COMPILE_ARM32=$(pwd)/toolchain/bin/$GCC_ARM32_FILE
-	  }
+		echo " "
+		echo "Using Linaro v6.5 toolchain"
+		echo " "
+
+		GCC_ARM64_FILE=aarch64-linux-gnu-
+		GCC_ARM32_FILE=arm-linux-gnueabi-
 	elif [ -e "toolchain/gcc4.9" ]
 	then
-	  {
-	     echo " "
-	     echo "Using Gcc v4.9 toolchain"
-	     echo " "
-	     GCC_ARM64_FILE=aarch64-linux-android-
-	     GCC_ARM32_FILE=arm-linux-gnueabi-
-	     export CROSS_COMPILE=$(pwd)/toolchain/bin/$GCC_ARM64_FILE
-	     export CROSS_COMPILE_ARM32=$(pwd)/toolchain/bin/$GCC_ARM32_FILE
-	  }
+		echo " "
+		echo "Using Gcc v4.9 toolchain"
+		echo " "
+
+		GCC_ARM64_FILE=aarch64-linux-android-
+		GCC_ARM32_FILE=arm-linux-gnueabi-
 	else
-	  echo " "
-	  echo "WARNING: Toolchain directory couldn't be found"
-	  echo " "
-	  sleep 2
-	  exit
+		echo " "
+		echo "WARNING: Toolchain directory couldn't be found"
+		echo " "
+		sleep 2
+		exit
 	fi
+
+	export CROSS_COMPILE=$(pwd)/toolchain/bin/$GCC_ARM64_FILE
+	export CROSS_COMPILE_ARM32=$(pwd)/toolchain/bin/$GCC_ARM32_FILE
 }
 
 ZIPPIFY()
 {
 	# Make Eureka flashable zip
-	
+
 	if [ -e "arch/$ARCH/boot/Image" ]
 	then
-	{
 		echo -e "*****************************************************"
 		echo -e "                                                     "
 		echo -e "       Building Eureka anykernel flashable zip       "
@@ -197,58 +187,46 @@ ZIPPIFY()
 		
 		# Go to anykernel directory
 		cd kernel_zip/anykernel
-		zip -r9 $ZIPNAME META-INF modules patch ramdisk tools anykernel.sh Image dtbo.img version
-		chmod 0777 $ZIPNAME
+		zip -r9 $ZIPPATH/$ZIPNAME META-INF modules patch ramdisk tools anykernel.sh Image dtbo.img version
+		chmod 0777 $ZIPPATH/$ZIPNAME
 		# Change back into kernel source directory
 		cd ..
 		sleep 1
 		cd ..
 		sleep 1
-	}
 	fi
 }
 
 AROMA_ZIP()
 {
 	# Make Eureka Aroma zip
-	
-	if [ -e "arch/$ARCH/boot/Image" ]
-	then
-	{
+
+	if [ -e "arch/$ARCH/boot/Image" ]; then
 		echo -e "*****************************************************"
 		echo -e "                                                     "
 		echo -e "         Building Eureka Aroma flashable zip         "
 		echo -e "                                                     "
 		echo -e "*****************************************************"
-		
+
 		# Copy Image and dtbo.img to aroma directory
 		cp -f arch/$ARCH/boot/dtbo.img kernel_zip/aroma/dtbo/main/dtbo.img
-		if [ "${TYPE}" == "oneui" ];
-		then                     
-		  {
-		      AROMA_DIR="oneui"
-		      AROMA_ZIP="oneui.zip"
-		  }
-		elif [ "${TYPE}" == "gsi" ];
-		then
-		  if [ "${SELINUX_B}" == "enforcing" ];
-		  then
-		     {
-		         AROMA_DIR="gsi_enf"
-		         AROMA_ZIP="gsi_enf.zip"
-		     }
-		  elif [ "${SELINUX_B}" == "permissive" ];
-		  then
-		     {
-		         AROMA_DIR="gsi_perm"
-		         AROMA_ZIP="gsi_perm.zip"
-		     }
-		   fi
+		if [ "${TYPE}" == "oneui" ]; then                     
+			AROMA_DIR="oneui"
+			AROMA_ZIP="oneui.zip"
+		elif [ "${TYPE}" == "gsi" ]; then
+			if [ "${SELINUX_B}" == "enforcing" ]; then
+				AROMA_DIR="gsi_enf"
+				AROMA_ZIP="gsi_enf.zip"
+			elif [ "${SELINUX_B}" == "permissive" ]; then
+				AROMA_DIR="gsi_perm"
+				AROMA_ZIP="gsi_perm.zip"
+			fi
 		fi
-		
+
 		echo " "
 		echo " Starting process for building Aroma zip for "$AROMA_DIR" "
 		echo " "
+
 		cp -f arch/$ARCH/boot/Image kernel_zip/aroma/aroma_files/$AROMA_DIR/Image
 		cd kernel_zip/aroma/aroma_files/$AROMA_DIR
 		zip -r9 $AROMA_ZIP META-INF modules patch ramdisk tools anykernel.sh Image version
@@ -257,60 +235,73 @@ AROMA_ZIP()
 		cd ..
 		cp -f aroma_files/$AROMA_DIR/$AROMA_ZIP kernel/$AROMA_ZIP
 		chmod 0777 kernel/$AROMA_ZIP
-		
+
 		zip -r9 Aroma_R"$REV"_"$DEVICE_Axxx".zip dtbo kernel magisk META-INF spectrum
 		chmod 0777 Aroma_R"$REV"_"$DEVICE_Axxx".zip
 		cd ..
 		sleep 1
 		cd ..
-	}
 	fi
 }
+
 PROCESSES()
 {
-	# Allow user to choose how many cores to be taken by compiler
-	echo "Your system has $CORES cores."
-	read -p "Please enter how many cores to be used by compiler (Leave blank to use all cores) : " cores;
-	if [ "${cores}" == "" ]; then
-		echo " "
-		echo "Using all $CORES cores for compilation"
-		sleep 2
-	else 
-		echo " "
-		echo "Using $cores cores for compilation "
-		CORES=$cores
-		sleep 2
+	if [ -z "$CIRCLECI" ]; then
+		# Allow user to choose how many cores to be taken by compiler
+
+		echo "Your system has $CORES cores."
+		read -p "Please enter how many cores to be used by compiler (Leave blank to use all cores) : " cores;
+		if [ "${cores}" == "" ]; then
+			echo " "
+			echo "Using all $CORES cores for compilation"
+			sleep 2
+		else
+			echo " "
+			echo "Using $cores cores for compilation "
+			CORES=$cores
+			sleep 2
+		fi
+	else
+		CORES=64
 	fi
 }
 
 ENTER_VERSION()
 {
-	# Enter kernel revision for this build.
-	read -p "Please type kernel version without R (E.g: 4.7) : " rev;
-	if [ "${rev}" == "" ]; then
-		echo " "
-		echo "     Using '$REV' as version"
-	else
-		REV=$rev
-		echo " "
-		echo "     Version = $REV"
+	if [ -z "$CIRCLECI" ]; then
+		# Enter kernel revision for this build.
+
+		read -p "Please type kernel version without R (E.g: 4.7) : " rev;
+		if [ "${rev}" == "" ]; then
+			echo " "
+			echo "     Using '$REV' as version"
+		else
+			REV=$rev
+			echo " "
+			echo "     Version = $REV"
+		fi
 	fi
 	sleep 2
 }
 
 USER()
 {
-	# Setup KBUILD_BUILD_USER
-	echo "Current build username is $USER"
-	echo " "
-	read -p "Please type build_user (E.g: Chatur) : " user;
-	if [ "${user}" == "" ]; then
+	if [ -z "$CIRCLECI" ]; then
+		# Setup KBUILD_BUILD_USER
+
+		echo "Current build username is $USER"
 		echo " "
-		echo "     Using '$USER' as Username"
+		read -p "Please type build_user (E.g: Chatur) : " user;
+		if [ "${user}" == "" ]; then
+			echo " "
+			echo "     Using '$USER' as Username"
+		else
+			export KBUILD_BUILD_USER=$user
+			echo " "
+			echo "     build_user = $user"
+		fi
 	else
-		export KBUILD_BUILD_USER=$user
-		echo " "
-		echo "     build_user = $user"
+		export KBUILD_BUILD_USER=CircleCI
 	fi
 	sleep 2
 }
@@ -318,84 +309,73 @@ USER()
 RENAME()
 {
 	# Give proper name to kernel and zip name
+
 	VERSION="Eureka_R"$REV"_"$DEVICE_Axxx"_"P/Q/R
 	ZIPNAME="Eureka_R"$REV"_"$DEVICE_Axxx"_"$TYPE"_"$SELINUX_STATUS""$ANDROID".zip"
 }
 
-function permissive() 
-{
-export SELINUX_B=permissive
-echo "Selected GSI and custom roms"
-sleep 2
-}
-
-function enforcing() 
-{
-export SELINUX_B=enforcing
-echo "Selected OneUI"
-sleep 2
-}
-
 SELINUX()
 {
-	# setup selinux for differents firmwares
-	echo -e "***************************************************************";
-	echo "             Select which version you wish to build               ";
-	echo -e "***************************************************************";
-	echo "Available versions:";
-	echo " "
-	echo "  1. Build OneUI version of Eureka with ENFORCING SElinux";
-	echo " "
-	echo "  2. Build GSI version of Eureka with PERMISSIVE SElinux";
-	echo " "
-	echo "  3. Build GSI version of Eureka with ENFORCING SElinux";
-	echo " "
-	echo "Leave empty to exit this script";
-	echo " "
-	echo " "
-	read -n 1 -p "Select your choice: " -s choice;
-	case ${choice} in
+	# Setup selinux for differents firmwares
+
+	if [ -z "$FORCESE" ]; then
+		echo -e "***************************************************************";
+		echo "             Select which version you wish to build               ";
+		echo -e "***************************************************************";
+		echo "Available versions:";
+		echo " "
+		echo "  1. Build OneUI version of Eureka with ENFORCING SElinux";
+		echo " "
+		echo "  2. Build GSI version of Eureka with PERMISSIVE SElinux";
+		echo " "
+		echo "  3. Build GSI version of Eureka with ENFORCING SElinux";
+		echo " "
+		echo "Leave empty to exit this script";
+		echo " "
+		echo " "
+
+		read -n 1 -p "Select your choice: " -s choice;
+	else
+		choice=$FORCESE
+	fi
+
+	case $choice in
 		1)
-		   {
 			export SELINUX_B=enforcing
 			export SELINUX_STATUS=""
 			export TYPE="oneui"
-		   };;
+			;;
 		2)
-		   {
-		   	export SELINUX_B=permissive
-		   	export SELINUX_STATUS="$SELINUX_B"_
-		   	export TYPE="gsi"
-		   };;
+			export SELINUX_B=permissive
+			export SELINUX_STATUS="$SELINUX_B"_
+			export TYPE="gsi"
+			;;
 		3)
-		   {
 			export SELINUX_B=enforcing
 			export SELINUX_STATUS="$SELINUX_B"_
 			export TYPE="gsi"
-		   };;
+			;;
 		*)
-		   {
 			echo
-			echo "Invalid choice entered. Exiting..."
+			echo "Invalid version entered. Exiting..."
 			sleep 2
 			exit 1
-		   };;
+			;;
 	esac
 
-	# setup selinux files
-	if [ ! -d security/selinux ]; then
-		mkdir security/selinux;
-	fi;
+	# Setup selinux files
 
-		if [ ${SELINUX_B} == "permissive" ]
-		then
+	if [ ! -d security/selinux ]; then
+		mkdir security/selinux
+	fi
+
+	if [ ${SELINUX_B} == "permissive" ]; then
 		echo "Using permissive selinux"
-			cp -rf $(pwd)/build_files/gsi/selinux security/
-		elif [ ${SELINUX_B} == "enforcing" ]
-		then
+		cp -rf $(pwd)/build_files/gsi/selinux security/
+	elif [ ${SELINUX_B} == "enforcing" ]; then
 		echo "Using enforcing selinux"
-			cp -rf $(pwd)/build_files/oneui/selinux security/
-		fi
+		cp -rf $(pwd)/build_files/oneui/selinux security/
+	fi
 	sleep 2
 }
 
@@ -404,24 +384,19 @@ MTP_FIX()
 	# This is needed for GSI version only !!
 	if [ ${TYPE} == "gsi" ]
 	then
-	{
-	   echo " "
-	   echo "       Setting up MTP for GSI.."
+		echo " "
+		echo "       Setting up MTP for GSI.."
 	   
-	   if [ ! -d drivers/usb/gadget ]; then
-		mkdir drivers/usb/gadget;
-	   fi;
-	   cp -rf $(pwd)/build_files/gsi/gadget drivers/usb/
-	   sleep 2
-	}
-	elif [ ${TYPE} == "oneui" ]
-	then
-	{
-	   if [ ! -d drivers/usb/gadget ]; then
-		mkdir drivers/usb/gadget;
-	   fi;
-	   cp -rf $(pwd)/build_files/oneui/gadget drivers/usb/
-	}
+		if [ ! -d drivers/usb/gadget ]; then
+			mkdir drivers/usb/gadget;
+		fi;
+		cp -rf $(pwd)/build_files/gsi/gadget drivers/usb/
+		sleep 2
+	elif [ ${TYPE} == "oneui" ]; then
+		if [ ! -d drivers/usb/gadget ]; then
+			mkdir drivers/usb/gadget
+		fi;
+		cp -rf $(pwd)/build_files/oneui/gadget drivers/usb/
 	fi
 }
 
@@ -449,12 +424,11 @@ DISPLAY_ELAPSED_TIME()
 	DIFF=$(($BUILD_END - $BUILD_START))
 
 	BUILD_SUCCESS=$?
-	if [ $BUILD_SUCCESS != 0 ]
-		then
-			echo " Error: Build failed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds $reset"
-			exit
+	if [ $BUILD_SUCCESS != 0 ]; then
+		echo " Error: Build failed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds $reset"
+		exit
 	fi
-	
+
 	echo -e "                     Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds $reset"
 	sleep 1
 }
@@ -468,12 +442,14 @@ COMMON_STEPS()
 	echo " Defconfig = $DEFCONFIG                              "
 	echo "                                                     "
 	echo "*****************************************************"
+
 	RENAME
 	sleep 1
 	echo " "	
 	BUILD_KERNEL
 	echo " "
 	sleep 1
+
 	ZIPPIFY
 	sleep 1
 	AROMA_ZIP
@@ -494,51 +470,53 @@ COMMON_STEPS()
 OS_MENU()
 {
 	# Give the choice to choose Android Version
-	PS3='
-Please select your Android Version: '
+
 	menuos=("$androidp" "$androidq" "$androidr" "Exit")
-	select menuos in "${menuos[@]}"
-	do
-	    case $menuos in
-        	"$androidp")
-			echo " "
-			echo "Android 9 (Pie) chosen as Android Major Version"
-			ANDROID=p
-			AND_VER=9
-			sleep 2
-			echo " "
-			break
-			;;
-		"$androidq")
-			echo " "
-			echo "Android 10 (Q) chosen as Android Major Version"
-			ANDROID=q
-			AND_VER=10
-			sleep 2
-			echo " "
-			break
-			;;
-		"$androidr")
-			echo " "
-			echo "Android 11 (R) chosen as Android Major Version"
-			ANDROID=r
-			AND_VER=11
-			sleep 2
-			echo " "
-			break
-			;;
-		"Exit")
-          		echo " "
-          		echo "Exiting build script.."
-          		sleep 2
-			echo " "
-          		exit
-            		;;
-        	*) 
-        		echo Invalid option.
-        		;;
+
+	ANDSEL()
+	{
+		case $menuos in
+			"$androidp")
+				echo " "
+				echo "Android 9 (Pie) chosen as Android Major Version"
+				ANDROID=p
+				AND_VER=9
+				;;
+			"$androidq")
+				echo " "
+				echo "Android 10 (Q) chosen as Android Major Version"
+				ANDROID=q
+				AND_VER=10
+				;;
+			"$androidr")
+				echo " "
+				echo "Android 11 (R) chosen as Android Major Version"
+				ANDROID=r
+				AND_VER=11
+				;;
+			*)
+				echo " "
+				echo "Exiting build script.."
+				exit
+				;;
 		esac
-	done
+	}
+	if [ ! -z "$FORCEANDROID" ] && [[ "$FORCEANDROID" =~ ^[0-9]+$ ]] && (( "$FORCEANDROID" > -1 )) && (( "$FORCEANDROID" < "${#menuos[@]}" )); then
+		menuos=${menuos[$FORCEANDROID]}
+		ANDSEL
+	else
+		echo " "
+		echo "Android versions available: "
+		echo " "
+
+		PS3='
+Please select your Android Version: '
+
+		select menuos in "${menuos[@]}"; do ANDSEL; break; done
+	fi
+
+	sleep 2
+	echo " "
 }
 
 
@@ -563,6 +541,11 @@ SELINUX
 clear
 MTP_FIX
 clear
+
+if [ -z "$ZIPPATH" ]; then
+	ZIPPATH=$(pwd)/kernel_zip/anykernel
+fi
+
 echo "******************************************************"
 echo "*             $PROJECT_NAME Build Script             *"
 echo "*                  Developer: Chatur                 *"
@@ -585,88 +568,57 @@ echo    "*  > ARM32 Toolchain exported                        *"
 echo -e "******************************************************"
 echo " "
 
-echo "Devices avalaible for compilation: "
-echo " "
-PS3='
-Please select your device: '
-menuoptions=("$SM_A105X" "$SM_A205X" "$SM_A305X" "$SM_A307X" "$SM_A405X" "$SM_A505X" "Exit")
-select menuoptions in "${menuoptions[@]}"
-do
-    case $menuoptions in
-        "$SM_A105X")
-        	echo " "
-        	echo "Android versions available: "
-        	echo " "
-		OS_MENU
-		echo " "
-		DEVICE_Axxx=$DEVICE_A105
-		DEFCONFIG=exynos7885-a10_"$TYPE"_"$SELINUX_STATUS"defconfig
-		COMMON_STEPS
-		break
-		;;
-        "$SM_A205X")
-		echo " "
-        	echo "Android versions available: "
-        	echo " "
-		OS_MENU
-		echo " "
-		DEVICE_Axxx=$DEVICE_A205
-		DEFCONFIG=exynos7885-a20_"$TYPE"_"$SELINUX_STATUS"defconfig
-		COMMON_STEPS
-		break
-		;;
-	"$SM_A305X")
-		echo " "
-        	echo "Android versions available: "
-        	echo " "
-		OS_MENU
-		echo " "
-		DEVICE_Axxx=$DEVICE_A305
-		DEFCONFIG=exynos7885-a30_"$TYPE"_"$SELINUX_STATUS"defconfig
-		COMMON_STEPS
-		break
-		;;
-	"$SM_A307X")
-		echo " "
-        	echo "Android versions available: "
-        	echo " "
-		OS_MENU
-		echo " "
-		DEVICE_Axxx=$DEVICE_A307
-		DEFCONFIG=exynos7885-a30s_"$TYPE"_"$SELINUX_STATUS"defconfig
-		COMMON_STEPS
-		break
-		;;
-	"$SM_A405X")
-		echo " "
-        	echo "Android versions available: "
-        	echo " "
-		OS_MENU
-		echo " "
-		DEVICE_Axxx=$DEVICE_A405
-		DEFCONFIG=exynos7885-a40_"$TYPE"_"$SELINUX_STATUS"defconfig
-		COMMON_STEPS
-		break
-		;;
-	"$SM_A505X")
-		echo " "
-        	echo "Android versions available: "
-        	echo " "
-		OS_MENU
-		echo " "
-		DEVICE_Axxx=$DEVICE_A505
-		DEFCONFIG=exynos9610-a50_"$TYPE"_"$SELINUX_STATUS"defconfig
-		COMMON_STEPS
-		break
-		;;
-	"Exit")
-            	echo " Exiting build script.."
-          	sleep 2
-          	exit
-          	;;
-        *) 
-        	echo Invalid option.
-        	;;
-    esac
-done
+devices=("$SM_A105X" "$SM_A205X" "$SM_A305X" "$SM_A307X" "$SM_A405X" "$SM_A505X" "Exit")
 
+DEVSEL()
+{
+	case $device in
+		"$SM_A105X")
+			DEVICE_Axxx=$DEVICE_A105
+			DEFCONFIG=exynos7885-a10_"$TYPE"_"$SELINUX_STATUS"defconfig
+			;;
+		"$SM_A205X")
+			DEVICE_Axxx=$DEVICE_A205
+			DEFCONFIG=exynos7885-a20_"$TYPE"_"$SELINUX_STATUS"defconfig
+			;;
+		"$SM_A305X")
+			DEVICE_Axxx=$DEVICE_A305
+			DEFCONFIG=exynos7885-a30_"$TYPE"_"$SELINUX_STATUS"defconfig
+			;;
+		"$SM_A307X")
+			DEVICE_Axxx=$DEVICE_A307
+			DEFCONFIG=exynos7885-a30s_"$TYPE"_"$SELINUX_STATUS"defconfig
+			;;
+		"$SM_A405X")
+			DEVICE_Axxx=$DEVICE_A405
+			DEFCONFIG=exynos7885-a40_"$TYPE"_"$SELINUX_STATUS"defconfig
+			;;
+		"$SM_A505X")
+			DEVICE_Axxx=$DEVICE_A505
+			DEFCONFIG=exynos9610-a50_"$TYPE"_"$SELINUX_STATUS"defconfig
+			;;
+		*)
+			echo " Exiting build script.."
+			sleep 2
+			exit
+			;;
+	esac
+}
+
+if [ ! -z "$FORCEDEVICE" ] && [[ "$FORCEDEVICE" =~ ^[0-9]+$ ]] && (( "$FORCEDEVICE" > -1 )) && (( "$FORCEDEVICE" < "${#devices[@]}" )); then
+	device=${devices[$FORCEDEVICE]}
+	DEVSEL
+else
+	echo "Devices avalaible for compilation: "
+	echo " "
+
+	PS3='
+Please select your device: '
+
+	select device in "${devices[@]}"; do DEVSEL; break; done
+fi
+
+echo "$device selected"
+OS_MENU
+echo " "
+COMMON_STEPS
